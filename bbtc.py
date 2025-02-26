c24031ce45eac10efd4683127548f4e5adc3b56c
AlexandreMRM <67433372+AlexandreMRM@users.noreply.github.com>
2025-02-26 17:47:00 -0300

    Update bbtc.py

--- bbtc.py
+++ bbtc.py
@@ -4,8 +4,8 @@ import streamlit as st
 
 
 
-@st.cache_resource(ttl=60000, show_spinner=False)
-def BD_Escala():
+@st.cache_data(ttl=600, show_spinner=False)
+def BD_Escala(data_ini=None, data_fim=None):
     # Parametros de Login AWS
     config = {
     'user': 'user_automation_jpa',
@@ -13,24 +13,43 @@ def BD_Escala():
     'host': 'comeia.cixat7j68g0n.us-east-1.rds.amazonaws.com',
     'database': 'test_phoenix_joao_pessoa'
     }
-    # Conexão as Views
     conexao = mysql.connector.connect(**config)
     cursor = conexao.cursor()
 
-    # Script MySql para requests
-    cursor.execute('''
+    query = '''
         SELECT 
-        `Tipo de Servico`,
-        `Data | Horario Apresentacao`,
-        `Escala`,
-        `Veiculo`,
-        `Motorista`,
-        `Guia`,
-        `Fornecedor Motorista`,
-        `Servico`,
-        `Apoio`
-        FROM vw_payment_guide;
-        ''')
+            `Tipo de Servico`,
+            `Data | Horario Apresentacao`,
+            `Data Execucao`,
+            COALESCE(NULLIF(`Voo`, ''), '-') AS `Voo`,
+            `Est. Origem`,
+            `Est. Destino`,
+            `Reserva`,
+            `Parceiro`,
+            `Total ADT`,
+            `Total CHD`,
+            `Observacao`,
+            `Escala`,
+            `Veiculo`,
+            `Motorista`,
+            `Guia`,
+            `Fornecedor Motorista`,
+            `Cliente`,
+            `Telefone Cliente`,
+            `Servico`,
+            `Apoio`,
+            CASE
+                WHEN `Tipo de Servico` = 'IN' THEN `Est. Destino`
+                ELSE `Est. Origem`
+            END AS `Estabelecimento`
+        FROM vw_payment_guide
+    '''
+    if data_ini and data_fim:
+        query += " WHERE `Data | Horario Apresentacao` BETWEEN %s AND %s"
+        cursor.execute(query, (data_ini, data_fim))
+    else:
+        cursor.execute(query)
+        
     # Coloca o request em uma variavel
     resultado = cursor.fetchall()
     # Busca apenas o cabecalhos do Banco
@@ -58,7 +77,17 @@ def split_apoio_data(row):
             'Motorista': parts[2].strip() if len(parts) > 2 else None,
             'Guia': parts[3].strip() if len(parts) > 3 else None,
             'Data | Horario Apresentacao': row['Data | Horario Apresentacao'],
-            'Servico': f'APOIO - '+ row['Servico']
+            'Servico': f'APOIO - '+ row['Servico'],
+            'Reserva': row['Reserva'],
+            'Voo': row['Voo'],
+            'Parceiro': row['Parceiro'],
+            'Total ADT': row['Total ADT'],
+            'Total CHD': row['Total CHD'],
+            'Observacao': row['Observacao'],
+            'Estabelecimento': row['Estabelecimento'],
+            'Cliente': row['Cliente'],
+            'Telefone Cliente': row['Telefone Cliente'],
+            'Data Execucao': row['Data Execucao']
         })
     else:
         # Return None for rows without 'Apoio' data to filter them out later
@@ -68,7 +97,17 @@ def split_apoio_data(row):
             'Motorista': None,
             'Guia': None,
             'Data | Horario Apresentacao': row['Data | Horario Apresentacao'],
-            'Servico': row['Servico']
+            'Servico': row['Servico'],
+            'Reserva': row['Reserva'],
+            'Voo': row['Voo'],
+            'Parceiro': row['Parceiro'],
+            'Total ADT': row['Total ADT'],
+            'Total CHD': row['Total CHD'],
+            'Observacao': row['Observacao'],
+            'Estabelecimento': row['Estabelecimento'],
+            'Cliente': row['Cliente'],
+            'Telefone Cliente': row['Telefone Cliente'],
+            'Data Execucao': row['Data Execucao']
         })
 
 
